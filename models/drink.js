d0bfcc310f00dfe68470c3772c2917dbe93e6240
Erick Yan <erick.yanzhang@gmail.com>
2022-07-29 18:33:42 -0400

    Extracted sentiment-based phrases and words, updated drink schema, updated mongodb pipeline

--- models/drink.js
+++ models/drink.js
@@ -7,7 +7,16 @@ const drinkSchema = new Schema({
     avgRating: {type: Number, required: true},
     drinkImage: {type: String, required: true},
     reviews: [{type: mongoose.Types.ObjectId, required: true, ref: 'Review'}],
-    shopId: {type: mongoose.Types.ObjectId, required: true, ref: 'Shop'}
+    shopId: {type: mongoose.Types.ObjectId, required: true, ref: 'Shop'},
+    shopName: {type: String, required: true},
+    positiveSents: [{type: String, required: false}],
+    negativeSents: [{type: String, required: false}],
+    happyScore: {type: Number, required: false},
+    surprisedScore: {type: Number, required: false},
+    sadScore: {type: Number, required: false},
+    angryScore: {type: Number, required: false},
+    positiveWords: [{type: String, required: false}],
+    negativeWords: [{type: Number, required: false}],
 });
 
 module.exports = mongoose.model('Drink', drinkSchema);
