da0377c56b751a655b64a177f2caf7dd85601fd3
David Chang <changoug@Gougbook-Air.local>
2022-08-03 19:27:42 -0400

    removed example, started experimental google review scraper

--- scraper-puppeteer/index.js
+++ scraper-puppeteer/index.js
@@ -1,8 +1,12 @@
-const browserObject = require('./browser');
-const scraperController = require('./pageController');
+const getReviews = require('./getReviews');
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+async function main() {
+  try {
+      const data = await getReviews("https://www.google.com/maps/place/Tour+Eiffel/@48.8583736,2.292298,17z/data=!4m5!3m4!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813");
+      console.log(JSON.stringify(data));
+  } catch(e) {
+      console.log(e);
+  }
+}
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance)
+main();
