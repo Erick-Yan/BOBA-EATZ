12a5d5f12a3768959a829400697b771e66e17321
David Chang <changoug@Gougbook-Air.local>
2022-08-08 12:08:17 -0400

    working on getting abstract webscraping method to work

--- scraper-puppeteer/getReviews.js
+++ scraper-puppeteer/getReviews.js
@@ -3,77 +3,69 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 puppeteer.use(StealthPlugin());
 
-const getReviews = async (url, shops, drinks) => {
-  const browser = await puppeteer.launch({headless: false, args: ['--disabled-setuid-sandbox', '--no-sandbox']});
+const getReviews = async (url, shops) => {
+  const browser = await puppeteer.launch({headless: false, args: ['--disabled-setuid-sandbox', '--no-sandbox', '--window-size=900,900']});
   const page = await browser.newPage();
   await page.goto(url);
   const data = [];
-  for (let i = 0; i < shops.length; i++) {
-    await searchShop 
-  }
-}
+  shops.map( async (item) => await searchShop(page, item));
+};
 
-const searchShop = async (url, shops, drinks) => {
-
-}
-
-const test = async (url, shops, drinks) => {
-  const browser = await puppeteer.launch({headless: false, args: ['--disabled-setuid-sandbox', '--no-sandbox']});
-  const page = await browser.newPage();
-  await page.goto(url);
-
+const searchShop = async (page, shop) => {
   await page.waitForSelector('#searchboxinput');
   await page.click('#searchboxinput');
-  await page.$eval('#searchboxinput', (el, shops) => { el.value = shops[0] }, shops);
+  await page.$eval('#searchboxinput', (el, shop) => { el.value = shop.name }, shop);
   await page.keyboard.press('Enter');
+  await page.waitForSelector('.hfpxzc');
+  const numberOfBranches = await page.evaluate( async () => {
+    return Array.from(document.querySelectorAll('.hfpxzc')).length;
+  });
+  for (let i = 0; i < numberOfBranches; i++) {
+    await searchBranch(page, shop, i * 2 + 3);
+  }
+}
+const searchBranch = async (page, shop,  branchIndex) => {
+  await page.waitForSelector(`div.m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd > div:nth-child(${branchIndex}) > div > a`);
+  await page.click(`div.m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd > div:nth-child(${branchIndex}) > div > a`);
+  shop.drinks.map( async (drink) => {
+    await searchDrink(page, drink);
+  });
+  await page.waitForTimeout(5000);
+  await page.waitForSelector('div.fKm1Mb > button');
+  await nativeClick(page, 'div.fKm1Mb > button');
 
-  await page.waitForSelector('.qBF1Pd');
-  
-  // click on first item
-  await page.click('.m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd > div:nth-child(5)');
+  // await page.click('div.fKm1Mb > button');
+};
 
-  await page.waitForSelector('.pV4rW.q8YqMd');
-
+const searchDrink = async (page, drink) => {
   //click on search button
-  await page.click('.pV4rW.q8YqMd');
-
+  await page.waitForSelector('.pV4rW.q8YqMd > div > button');
+  await nativeClick(page, '.pV4rW.q8YqMd > div > button');
   // search input
-   await page.$eval('div.MrFZRe.g8q29e > div > input', (el, drinks) => { el.value = drinks[0] }, drinks);
-   await page.keyboard.press('Enter');
-   await page.waitForTimeout(1000);
-   await page.click('.hWERUb');
-   await page.waitForTimeout(1000);
-   await page.click('.fKm1Mb');
-   console.log('gud stuff');
+  await page.waitForSelector('div.MrFZRe.g8q29e > div > input');
+  await page.$eval('div.MrFZRe.g8q29e > div > input', (el, drink) => { el.value = drink }, drink);
+  await page.keyboard.press('Enter');
+  await page.waitForTimeout(5000);
+  await page.waitForSelector('div.hWERUb > button');
+  await nativeClick(page, 'div.hWERUb > button');
+
+  // await page.click('div.hWERUb > button');
+};
+
+const getDrinkReviews = async () => {
+};
 
 
-//   await page.waitForSelector();
-
-  // const data = await page.evaluate(() => {
-  //     let reviewAuthorNamesClasses = document.getElementsByClassName('section-review-title');
-  //     let reviewAuthorNames = [];
-  //     for (let elements of reviewAuthorNamesClasses) {
-  //         reviewAuthorNames.push(elements.innerText);
-  //     }
-  //     return {
-  //         reviewAuthorNames
-  //     }
-  // })
-  // browser.close();
-  // return new Promise((resolve, reject) => {
-  //     resolve(data);
-  //     if(reject) {
-  //         reject({error: "error while scraping data."})
-  //     }
-  // })
-
+const nativeClick = async(page, selector) => {
+  const button = await page.$(selector);
+  await button.evaluate(b => b.click());
 };
 
 async function scrollPage(page, scrollContainer) {
     let lastHeight = await page.evaluate(`document.querySelector("${scrollContainer}").scrollHeight`);
     while (true) {
       await page.evaluate(`document.querySelector("${scrollContainer}").scrollTo(0, document.querySelector("${scrollContainer}").scrollHeight)`);
-      await page.waitForTimeout(2000);
+      await page.waitForTimeout(3000);
       let newHeight = await page.evaluate(`document.querySelector("${scrollContainer}").scrollHeight`);
       if (newHeight === lastHeight) {
         break;
--- scraper-puppeteer/index.js
+++ scraper-puppeteer/index.js
@@ -1,11 +1,15 @@
 const getReviews = require('./getReviews');
 
 async function main() {
-  const shops = ['CoCo Fresh Tea & Juice'];
-  const drinks = ['3 Guys'];
+  const shops = [
+    {
+      name: 'CoCo Fresh Tea & Juice',
+      drinks: ['3 Guys']
+    }
+  ];
   try {
-      const data = await getReviews('https://www.google.com/maps/', shops, drinks);
-      console.log(JSON.stringify(data));
+      const data = await getReviews('https://www.google.com/maps/@43.6764672,-79.3935872,12z', shops);
+      // console.log(JSON.stringify(data));
   } catch(e) {
       console.log(e);
   }
